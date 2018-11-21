dd20a67354f928fffd291965425a2e0934f0c4a1
Konrad Dysput <konrad.dysput@gmail.com>
2018-11-21 19:08:54 +0100

    Package.json + removed file writer

--- src/Model/BacktraceUnhandledException.cs
+++ src/Model/BacktraceUnhandledException.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 
 namespace Backtrace.Unity.Model
 {
@@ -45,47 +44,36 @@ namespace Backtrace.Unity.Model
             var frames = _stacktrace.Trim().Split('\n');
             foreach (var frameString in frames)
             {
-                using (var outputFile = new StreamWriter(Path.Combine(@"C:\Users\konra\source\BacktraceLogs\", $"errors-unhandled-stacktrace.txt"), true))
+                int methodNameEndIndex = frameString.IndexOf(')');
+                //methodname index should be greater than 0 AND '(' should be before ')'
+                if (methodNameEndIndex < 1 && frameString[methodNameEndIndex - 1] != '(')
                 {
-                    try
-                    {
-                        outputFile.WriteLine("STACK FRAME : " + frameString.ToString());
-                        int methodNameEndIndex = frameString.IndexOf(')');
-                        //methodname index should be greater than 0 AND '(' should be before ')'
-                        if (methodNameEndIndex < 1 && frameString[methodNameEndIndex - 1] != '(')
-                        {
-                            //invalid stack frame
-                            return;
-                        }
-                        string routingPaths = frameString.Substring(0, methodNameEndIndex - 1);
-                        var routingParams = routingPaths.Trim().Split('.');
-                        string methodPath = string.Empty;
-                        int fileLine = 0;
-
-                        int sourceInformationStartIndex = frameString.IndexOf('(', methodNameEndIndex + 1);
-                        if (sourceInformationStartIndex > -1)
-                        {
-                            // -1 because we don't want additional ')' in the end of the string
-                            int sourceStringLength = frameString.Length - sourceInformationStartIndex - 1;
-                            string sourceString =
-                                frameString.Substring(sourceInformationStartIndex, sourceStringLength);
-                            var sourceInfo = sourceString.Split(':');
-                            methodPath = sourceInfo[0].Substring(sourceInfo[0].IndexOf(' '));
-                            int.TryParse(sourceInfo[1], out fileLine);
-                        }
-                        StackFrames.Add(new BacktraceStackFrame()
-                        {
-                            FunctionName = routingParams[1],
-                            Library = routingParams[0],
-                            Line = fileLine,
-                            SourceCode = methodPath
-                        });
-                    }
-                    catch(Exception e)
-                    {
-                        outputFile.WriteLine(e.ToString());
-                    }
+                    //invalid stack frame
+                    return;
                 }
+                string routingPaths = frameString.Substring(0, methodNameEndIndex - 1);
+                var routingParams = routingPaths.Trim().Split('.');
+                string methodPath = string.Empty;
+                int fileLine = 0;
+
+                int sourceInformationStartIndex = frameString.IndexOf('(', methodNameEndIndex + 1);
+                if (sourceInformationStartIndex > -1)
+                {
+                    // -1 because we don't want additional ')' in the end of the string
+                    int sourceStringLength = frameString.Length - sourceInformationStartIndex - 1;
+                    string sourceString =
+                        frameString.Substring(sourceInformationStartIndex, sourceStringLength);
+                    var sourceInfo = sourceString.Split(':');
+                    methodPath = sourceInfo[0].Substring(sourceInfo[0].IndexOf(' '));
+                    int.TryParse(sourceInfo[1], out fileLine);
+                }
+                StackFrames.Add(new BacktraceStackFrame()
+                {
+                    FunctionName = routingParams[1],
+                    Library = routingParams[0],
+                    Line = fileLine,
+                    SourceCode = methodPath
+                });
 
             }
         }
