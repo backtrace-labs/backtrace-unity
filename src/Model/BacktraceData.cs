c5f3ad8d3cc6ab272be643beea4713abcd6542fd
Konrad Dysput <konrad.dysput@gmail.com>
2020-02-06 18:08:02 -0800

    Version update

--- src/Model/BacktraceData.cs
+++ src/Model/BacktraceData.cs
@@ -123,7 +123,7 @@ namespace Backtrace.Unity.Model
                 ["lang"] = "csharp",
                 ["langVersion"] = "Unity",
                 ["agent"] = "backtrace-unity",
-                ["agentVersion"] = "1.1.5",
+                ["agentVersion"] = "2.0.0-alpha",
                 ["mainThread"] = MainThread,
                 ["classifiers"] = new JArray(Classifier),
                 ["attributes"] = Attributes.ToJson(),
@@ -169,7 +169,7 @@ namespace Backtrace.Unity.Model
             Uuid = Report.Uuid;
             Timestamp = Report.Timestamp;
             LangVersion = "Mono/IL2CPP";
-            AgentVersion = "1.1.5";
+            AgentVersion = "2.0.0-alpha";
             Classifier = Report.ExceptionTypeReport ? new[] { Report.Classifier } : null;
         }
     }
