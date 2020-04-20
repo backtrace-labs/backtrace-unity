b3d8ff59798be9bcca4bee1349c08f0f82e1aa15
konraddysput <konrad.dysput@gmail.com>
2020-04-20 14:16:56 +0200

    Version 3.0.0 - alpha

--- Runtime/Model/BacktraceData.cs
+++ Runtime/Model/BacktraceData.cs
@@ -154,7 +154,7 @@ namespace Backtrace.Unity.Model
             LangVersion = "Mono";
 #endif
 
-            AgentVersion = "3.0.0";
+            AgentVersion = "3.0.0-alpha";
             Classifier = Report.ExceptionTypeReport ? new[] { Report.Classifier } : null;
         }
     }
