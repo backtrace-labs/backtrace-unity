e388e5fcad80044e05d41eb584afce5d3865788a
konraddysput <konrad.dysput@gmail.com>
2020-06-10 21:05:24 +0200

    Fix

--- Runtime/Model/JsonData/Annotations.cs
+++ Runtime/Model/JsonData/Annotations.cs
@@ -1,95 +1,183 @@
-﻿using Backtrace.Unity.Extensions;
-using Backtrace.Unity.Types;
+﻿using Backtrace.Unity.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace Backtrace.Unity.Model
+namespace Backtrace.Unity.Model.JsonData
 {
-    public class DeduplicationModel
+    /// <summary>
+    /// Get report annotations - environment variables
+    /// </summary>
+    public class Annotations
     {
-        private readonly BacktraceData _backtraceData;
-        private readonly DeduplicationStrategy _strategy;
-        public DeduplicationModel(
-            BacktraceData backtraceData,
-            DeduplicationStrategy strategy)
-        {
-            _backtraceData = backtraceData;
-            _strategy = strategy;
-        }
-        public string StackTrace
+
+        private static Dictionary<string, string> _variables;
+
+        /// <summary>
+        /// System environment values dictionary
+        /// </summary>
+        internal static Dictionary<string, string> Variables
         {
             get
             {
-                if (_strategy == DeduplicationStrategy.None)
+                if (_variables == null)
                 {
-                    return "";
+                    _variables = new Dictionary<string, string>();
+                    foreach (DictionaryEntry variable in Environment.GetEnvironmentVariables())
+                    {
+                        _variables.Add(variable.Key.ToString(), Regex.Escape(variable.Value.ToString() ?? "NULL"));
+                    }
                 }
-                if (_backtraceData.Report == null || _backtraceData.Report.DiagnosticStack == null)
-                {
-                    return "";
-                }
-                var result = _backtraceData.Report.DiagnosticStack
-                    .Select(n => n.FunctionName)
-                    .OrderByDescending(n => n);
 
-                var stackTrace = new HashSet<string>(result).ToArray();
-                return string.Join(",", stackTrace);
-            }
-        }
-        public string Classifier
-        {
-            get
-            {
-                if ((_strategy & DeduplicationStrategy.Classifier) == 0)
-                {
-                    return "";
-                }
-                var classifier = _backtraceData.Classifier ?? System.Array.Empty<string>();
-                return string.Join(",", classifier);
-            }
-        }
-        public string ExceptionMessage
-        {
-            get
-            {
-                if ((_strategy & DeduplicationStrategy.Message) == 0)
-                {
-                    return string.Empty;
-                }
-                if (_backtraceData.Report == null || string.IsNullOrEmpty(_backtraceData.Report.Message))
-                {
-                    return string.Empty;
-                }
-                return _backtraceData.Report.Message.OnlyLetters();
+                return _variables;
             }
         }
 
-        public string Factor
+        /// <summary>
+        /// Set maximum number of game objects in Backtrace report
+        /// </summary>
+        private readonly int _gameObjectDepth;
+
+        /// <summary>
+        /// Exception object
+        /// </summary>
+        private Exception _exception { get; set; }
+
+        public Annotations()
         {
-            get
-            {
-                if (_backtraceData.Report == null)
-                {
-                    return string.Empty;
-                }
-                return _backtraceData.Report.Factor;
-            }
+        }
+        /// <summary>
+        /// Create new instance of Annotations class
+        /// </summary>
+        /// <param name="exception">Current exception</param>
+        /// <param name="gameObjectDepth">Game object depth</param>
+        public Annotations(Exception exception, int gameObjectDepth)
+        {
+            _gameObjectDepth = gameObjectDepth;
+            _exception = exception;
         }
 
-        public string GetSha()
+        public BacktraceJObject ToJson()
         {
-            if (!string.IsNullOrEmpty(_backtraceData.Report.Fingerprint))
+            var annotations = new BacktraceJObject();
+            var envVariables = new BacktraceJObject();
+
+            foreach (var envVariable in Variables)
             {
-                return _backtraceData.Report.Fingerprint;
+                envVariables[envVariable.Key] = envVariable.Value;
+            }
+            annotations["Environment Variables"] = envVariables;
+
+            if (_exception != null)
+            {
+                annotations["Exception properties"] = new BacktraceJObject()
+                {
+                    ["message"] = _exception.Message,
+                    ["stackTrace"] = _exception.StackTrace,
+                    ["type"] = _exception.GetType().FullName,
+                    ["source"] = _exception.Source
+                };
             }
 
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append(ExceptionMessage);
-            stringBuilder.Append(Classifier);
-            stringBuilder.Append(StackTrace);
+            if (_gameObjectDepth > -1)
+            {
+                var activeScene = SceneManager.GetActiveScene();
 
-            return stringBuilder.GetSha();
+                var gameObjects = new List<BacktraceJObject>();
+
+                var rootObjects = new List<GameObject>();
+                activeScene.GetRootGameObjects(rootObjects);
+                foreach (var gameObject in rootObjects)
+                {
+                    gameObjects.Add(ConvertGameObject(gameObject));
+                }
+                annotations["Game objects"] = gameObjects;
+            }
+
+
+            return annotations;
+        }
+
+        private BacktraceJObject ConvertGameObject(GameObject gameObject, int depth = 0)
+        {
+            if (gameObject == null)
+            {
+                return new BacktraceJObject();
+            }
+            var jGameObject = GetJObject(gameObject);
+            var innerObjects = new List<BacktraceJObject>();
+
+            foreach (var childObject in gameObject.transform)
+            {
+                var transformChildObject = childObject as Component;
+                if (transformChildObject == null)
+                {
+                    continue;
+                }
+                innerObjects.Add(ConvertGameObject(transformChildObject, gameObject.name, depth + 1));
+            }
+            jGameObject["children"] = innerObjects;
+            return jGameObject;
+        }
+
+        private BacktraceJObject ConvertGameObject(Component gameObject, string parentName, int depth)
+        {
+            if (_gameObjectDepth > 0 && depth > _gameObjectDepth)
+            {
+                return new BacktraceJObject();
+            }
+            var result = GetJObject(gameObject, parentName);
+            if (_gameObjectDepth > 0 && depth + 1 >= _gameObjectDepth)
+            {
+                return result;
+            }
+            var innerObjects = new List<BacktraceJObject>();
+
+
+            foreach (var childObject in gameObject.transform)
+            {
+                var transformChildObject = childObject as Component;
+                if (transformChildObject == null)
+                {
+                    continue;
+                }
+                innerObjects.Add(ConvertGameObject(transformChildObject, gameObject.name, depth + 1));
+            }
+            result["children"] = innerObjects;
+            return result;
+        }
+
+        private BacktraceJObject GetJObject(GameObject gameObject, string parentName = "")
+        {
+            var o = new BacktraceJObject();
+            o["name"] = gameObject.name;
+            o["isStatic"] = gameObject.isStatic;
+            o["layer"] = gameObject.layer;
+            o["transform.position"] = gameObject.transform.position.ToString() ?? "";
+            o["transform.rotation"] = gameObject.transform.rotation.ToString() ?? "";
+            o["tag"] = gameObject.tag;
+            o["activeInHierarchy"] = gameObject.activeInHierarchy;
+            o["activeSelf"] = gameObject.activeSelf;
+            o["hideFlags"] = (int)gameObject.hideFlags;
+            o["instanceId"] = gameObject.GetInstanceID();
+            o["parnetName"] = string.IsNullOrEmpty(parentName) ? "root object" : parentName;
+            return o;
+        }
+
+        private BacktraceJObject GetJObject(Component gameObject, string parentName = "")
+        {
+            var o = new BacktraceJObject();
+            o["name"] = gameObject.name;
+            o["transform.position"] = gameObject.transform.position.ToString() ?? "";
+            o["transform.rotation"] = gameObject.transform.rotation.ToString() ?? "";
+            o["tag"] = gameObject.tag;
+            o["hideFlags"] = (int)gameObject.hideFlags;
+            o["instanceId"] = gameObject.GetInstanceID();
+            o["parnetName"] = string.IsNullOrEmpty(parentName) ? "root object" : parentName;
+            return o;
         }
     }
 }
--- Runtime/Types/DeduplicationStrategy.cs
+++ Runtime/Types/DeduplicationStrategy.cs
@@ -1,149 +1,49 @@
-﻿using Backtrace.Unity.Model;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-namespace Backtrace.Unity.Services
+namespace Backtrace.Unity.Types
 {
+
     /// <summary>
-    /// Report watcher class. Watcher controls number of reports sending per one minute. If value reportPerMin is equal to zero, there is no request sending to API. Value has to be greater than or equal to 0
+    /// Determine deduplication strategy
     /// </summary>
-    public class ReportLimitWatcher
+    [Flags]
+    public enum DeduplicationStrategy
     {
         /// <summary>
-        /// Report timestamp queue. ReportLimitWatcher store events timestamp in _reportQueue
-        /// to validate number of reports that Backtarce integration will send per minute.
+        /// Ignore deduplication strategy
         /// </summary>
-        internal readonly Queue<long> _reportQueue;
+        [Tooltip("Deduplication rules are disabled.")]
+#if UNITY_2019_2_OR_NEWER
+        [InspectorName("Disable")]
+#endif
+        None = 0,
 
         /// <summary>
-        /// Time period used to clear values from report queue.
+        /// Only stack trace
         /// </summary>
-        private readonly long _queueReportTime = 60;
+        [Tooltip("Faulting callstack - use the faulting callstack as a factor in client-side rate limiting.")]
+#if UNITY_2019_2_OR_NEWER
+        [InspectorName("Faulting callstack")]
+#endif
+        Default = 1,
 
         /// <summary>
-        /// Determine if watcher is enabled.
+        /// Stack trace and exception type
         /// </summary>
-        private bool _watcherEnable;
-        /// <summary>
-        /// Determine how many reports class instance can store in report queue.
-        /// </summary>
-        private int _reportPerMin;
-
-
-        /// <summary>
-        /// Determine if ReportLimitWatcher class should display warning message
-        /// </summary>
-        private bool _displayMessage = false;
+        [Tooltip("Unity by default will validate ssl certificates. By using this option you can avoid ssl certificates validation. However, if you don't need to ignore ssl validation, please set this option to false.", order = 0)]
+#if UNITY_2019_2_OR_NEWER
+        [InspectorName("Exception type")]
+#endif
+        Classifier = 2,
 
         /// <summary>
-        /// Determine if BacktraceClient/BacktraceDatabase hit report limit 
+        /// Stack trace and exception message
         /// </summary>
-        private bool _limitHit = false;
-
-        /// <summary>
-        /// Create new instance of background watcher
-        /// </summary>
-        /// <param name="reportPerMin">How many times per minute watcher can send a report</param>
-        internal ReportLimitWatcher(uint reportPerMin)
-        {
-            if (reportPerMin < 0)
-            {
-                throw new ArgumentException(string.Format((string)"{0} have to be greater than or equal to zero",
-                    (object)"reportPerMin"));
-            }
-            int reportNumber = checked((int)reportPerMin);
-            _reportQueue = new Queue<long>(reportNumber);
-            _reportPerMin = reportNumber;
-            _watcherEnable = reportPerMin != 0;
-        }
-
-        internal void SetClientReportLimit(uint reportPerMin)
-        {
-            int reportNumber = checked((int)reportPerMin);
-            _reportPerMin = reportNumber;
-            _watcherEnable = reportPerMin != 0;
-        }
-
-
-        /// <summary>
-        /// Check if user can send new report to a Backtrace API
-        /// </summary>
-        /// <param name="report">Current report</param>
-        /// <returns>true if user can add a new report</returns>
-        public bool WatchReport(long timestamp, bool displayMessageOnLimitHit = true)
-        {
-            if (!_watcherEnable)
-            {
-                return true;
-            }
-            //clear all reports older than _queReportTime
-            Clear();
-            if (_reportQueue.Count + 1 > _reportPerMin)
-            {
-                _limitHit = true;
-                if (displayMessageOnLimitHit)
-                {
-                    DisplayReportLimitHitMessage();
-                }
-                return false;
-            }
-            _limitHit = false;
-            _displayMessage = true;
-            _reportQueue.Enqueue(timestamp);
-            return true;
-        }
-
-        /// <summary>
-        /// Check if user can send new report to a Backtrace API
-        /// </summary>
-        /// <param name="report">Current report</param>
-        /// <returns>true if user can add a new report</returns>
-        public bool WatchReport(BacktraceReport report, bool displayMessageOnLimitHit = true)
-        {
-            return WatchReport(report.Timestamp, displayMessageOnLimitHit);
-        }
-
-
-        /// <summary>
-        /// Display report limit hit 
-        /// </summary>
-        private void DisplayReportLimitHitMessage()
-        {
-            if (_limitHit && _displayMessage)
-            {
-                _displayMessage = false;
-                Debug.LogWarning(string.Format("Backtrace report limit hit({0}/min) – Ignoring errors for 1 minute",
-                    _reportPerMin));
-            }
-        }
-
-
-        /// <summary>
-        /// Remove all records with timestamp older than one minute from now
-        /// </summary>
-        private void Clear()
-        {
-            long currentTime = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
-            bool clear = false;
-            while (!clear && _reportQueue.Count != 0)
-            {
-                var item = _reportQueue.Peek();
-                clear = !(currentTime - item >= _queueReportTime);
-                if (!clear)
-                {
-                    _reportQueue.Dequeue();
-                }
-            }
-        }
-
-        /// <summary>
-        /// This method only is used in test class project. Use Reset method to reset current counter and available reports
-        /// </summary>
-        internal void Reset()
-        {
-            _reportQueue.Clear();
-        }
-
+        [Tooltip("Unity by default will validate ssl certificates. By using this option you can avoid ssl certificates validation. However, if you don't need to ignore ssl validation, please set this option to false.", order = 0)]
+#if UNITY_2019_2_OR_NEWER
+        [InspectorName("Exception message")]
+#endif
+        Message = 4
     }
 }
