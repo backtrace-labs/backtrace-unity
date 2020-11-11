dd9c74e9d660a13edff0570b051f644d042c14d9
konraddysput <konrad.dysput@gmail.com>
2020-11-11 15:35:45 +0100

    Version update

--- Runtime/Model/BacktraceData.cs
+++ Runtime/Model/BacktraceData.cs
@@ -45,7 +45,7 @@ namespace Backtrace.Unity.Model
         /// <summary>
         /// Version of the C# library
         /// </summary>
-        public const string AgentVersion = "3.2.0-rc3";
+        public const string AgentVersion = "3.2.0";
 
         /// <summary>
         /// Application thread details
