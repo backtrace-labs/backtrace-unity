add569bb40245fcc4a7d35ddf62b12a47c2c77e1
konraddysput <konrad.dysput@gmail.com>
2021-03-02 14:22:34 +0100

    Release notes + version update

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -20,7 +20,7 @@ namespace Backtrace.Unity
     {
         public BacktraceConfiguration Configuration;
 
-        public const string VERSION = "3.3.2";
+        public const string VERSION = "3.3.3";
         public bool Enabled { get; private set; }
 
         /// <summary>
