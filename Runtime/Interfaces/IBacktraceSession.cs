e48d95147412c5ff5b16ae9b5e7c07fc640591ba
kdysput <konrad.dysput@gmail.com>
2021-05-11 10:50:48 +0200

    Adjusted API

--- Runtime/Interfaces/IBacktraceSession.cs
+++ Runtime/Interfaces/IBacktraceSession.cs
@@ -6,7 +6,12 @@ namespace Backtrace.Unity.Interfaces
     public interface IBacktraceSession
     {
         /// <summary>
-        /// List of unique events stored in the BacktraceSession class
+        /// This list contains the unique events which will be sent whenever Send is triggered (manually or automatically). 
+        /// By default, this list contains the guid attribute. This list is mutable, which means the default can be removed
+        /// and custom unique events can be added. Typical values to add are UserID, SteamID and other attributes that uniquely 
+        /// identify an user. This list is persistent, meaning that events will not be removed upon Send() (like for summation events).
+        /// For non-standard unique events, server side configuration needs to be done. 
+        /// Please refer to the <see href="https://support.backtrace.io">online documentation</see>.
         /// </summary>
         LinkedList<UniqueEvent> UniqueEvents { get; }
 
@@ -29,11 +34,29 @@ namespace Backtrace.Unity.Interfaces
         /// Send startup event to Backtrace
         /// </summary>
         void SendStartupEvent();
+
+        /// <summary>
+        /// Trigger a manual send, will send all outgoing messages currently queued.
+        /// </summary>
         void Send();
         bool AddUniqueEvent(string attributeName);
-        bool AddSessionEvent(string sessionEvent);
         bool AddUniqueEvent(string attributeName, IDictionary<string, string> attributes);
-        bool AddSessionEvent(string sessionEvent, IDictionary<string, string> attributes);
+
+        /// <summary>
+        /// Adds a summation event to the outgoing queue.
+        /// </summary>
+        /// See <see cref="IBacktraceMetricsClient.Send(string, IDictionary)"/>.
+        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://support.backtrace.io" > online documentation</see>.</param>
+        /// <returns>true if added successfully, otherwise false.</returns>
+        bool AddSessionEvent(string metricsGroupName);
+
+        /// <summary>
+        /// Adds a summation event to the outgoing queue.
+        /// </summary>
+        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://support.backtrace.io" > online documentation</see>.</param>
+        /// <param name="attributes">Custom attributes to add. Will be merged with the default attributes, with attribute values provided here overriding any defaults.</param>
+        /// <returns>true if added successfully, otherwise false.</returns>
+        bool AddSessionEvent(string metricsGroupName, IDictionary<string, string> attributes);
         void Tick(float time);
     }
 }
