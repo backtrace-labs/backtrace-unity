8efd97e379da944cb1c7671792e2bd2cc28b6daf
kdysput <konrad.dysput@gmail.com>
2021-06-01 19:20:41 +0200

    version update

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -23,7 +23,7 @@ namespace Backtrace.Unity
     /// </summary>
     public class BacktraceClient : MonoBehaviour, IBacktraceClient
     {
-        public const string VERSION = "3.5.0";
+        public const string VERSION = "3.5.0-preview.1";
 
         public BacktraceConfiguration Configuration;
 
