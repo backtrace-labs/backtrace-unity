b51f43f1de2e2ca37ca364efa624212616bfc55b
kdysput <konrad.dysput@gmail.com>
2021-10-19 13:36:01 +0200

    Fixed unit tests after merge + fixed .meta file creation + improved code comments

--- Runtime/Model/BacktraceConfiguration.cs
+++ Runtime/Model/BacktraceConfiguration.cs
@@ -56,12 +56,12 @@ namespace Backtrace.Unity.Model
         public bool DestroyOnLoad = false;
 
         /// <summary>
-        /// Sampling configuration - fractional sampling allows to drop some % of unhandled exception.
+        /// Sampling configuration - fractional sampling allows to drop some % of Unity errors.
         /// </summary>
-        [Tooltip("Log random sampling rate - Enables a random sampling mechanism for unhandled exceptions - by default sampling is equal to 0.01 - which means only 1% of randomply sampling reports will be send to Backtrace. \n" +
-            "* 1 - means 100% of unhandled exception reports will be reported by library,\n" +
-            "* 0.1 - means 10% of unhandled exception reports will be reported by library,\n" +
-            "* 0 - means library is going to drop all unhandled exception.")]
+        [Tooltip("Log random sampling rate - Enables a random sampling mechanism for Unity.Error logs - by default sampling is equal to 0.01 - which means only 1% of randomply sampling reports will be send to Backtrace. \n" +
+            "* 1 - means 100% of error reports will be reported by library,\n" +
+            "* 0.1 - means 10% of error reports will be reported by library,\n" +
+            "* 0 - means library is going to drop all errors.")]
         [Range(0, 1)]
         public double Sampling = 0.01d;
 
