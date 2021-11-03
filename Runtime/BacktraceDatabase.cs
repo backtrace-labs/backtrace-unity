845a60a627bcaf212b4f287cd0254e78ca694153
kdysput <konrad.dysput@gmail.com>
2021-11-03 23:05:57 +0100

    Do not ignore EnableBreadcurmbsSupport flag

--- Runtime/BacktraceDatabase.cs
+++ Runtime/BacktraceDatabase.cs
@@ -36,7 +36,7 @@ namespace Backtrace.Unity
             {
                 if (_breadcrumbs == null)
                 {
-                    if (Enable && BacktraceBreadcrumbs.CanStoreBreadcrumbs(Configuration.LogLevel, Configuration.BacktraceBreadcrumbsLevel))
+                    if (Enable && Configuration.EnableBreadcrumbsSupport && BacktraceBreadcrumbs.CanStoreBreadcrumbs(Configuration.LogLevel, Configuration.BacktraceBreadcrumbsLevel))
                     {
                         _breadcrumbs = new BacktraceBreadcrumbs(new BacktraceStorageLogManager(Configuration.GetFullDatabasePath()));
                     }
