0ee82923f32c55ae1987f8d051313cc7a9382bef
Sebastian Alex <sebastian.alex@saucelabs.com>
2022-05-06 08:28:00 -0700

    Updates version to 3.7.6-preview.1.

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -24,7 +24,7 @@ namespace Backtrace.Unity
     /// </summary>
     public class BacktraceClient : MonoBehaviour, IBacktraceClient
     {
-        public const string VERSION = "3.7.5";
+        public const string VERSION = "3.7.6-preview.1";
         internal const string DefaultBacktraceGameObjectName = "BacktraceClient";
         public BacktraceConfiguration Configuration;
 
