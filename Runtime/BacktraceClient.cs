d4a6172aac6cfadafccd3582125084ad8c8e4a13
Konrad Dysput <konrad.dysput@gmail.com>
2023-11-02 21:36:41 +0100

    Version update

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -24,7 +24,7 @@ namespace Backtrace.Unity
     /// </summary>
     public class BacktraceClient : MonoBehaviour, IBacktraceClient
     {
-        public const string VERSION = "3.8.1";
+        public const string VERSION = "3.8.2";
         internal const string DefaultBacktraceGameObjectName = "BacktraceClient";
         public BacktraceConfiguration Configuration;
 
