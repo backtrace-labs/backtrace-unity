3623fa03887f3ac667e7885d7f852ab3302e72b9
Konrad Dysput <konrad.dysput@saucelabs.com>
2024-12-11 17:22:55 +0100

    Change a version to 3.11.0

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -24,7 +24,7 @@ namespace Backtrace.Unity
     /// </summary>
     public class BacktraceClient : MonoBehaviour, IBacktraceClient
     {
-        public const string VERSION = "3.10.1";
+        public const string VERSION = "3.11.0";
         internal const string DefaultBacktraceGameObjectName = "BacktraceClient";
         public BacktraceConfiguration Configuration;
 
