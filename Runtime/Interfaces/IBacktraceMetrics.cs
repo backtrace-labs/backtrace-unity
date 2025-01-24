1d340fc9302cd68b28438b2c56da2617df40040d
Konrad Dysput <konrad.dysput@gmail.com>
2025-01-24 14:53:15 +0100

    Change documentation url to docs.saucelabs.com (#239)
    Co-authored-by: Konrad Dysput <konrad.dysput@saucelabs.com>

--- Runtime/Interfaces/IBacktraceMetrics.cs
+++ Runtime/Interfaces/IBacktraceMetrics.cs
@@ -13,7 +13,7 @@ namespace Backtrace.Unity.Interfaces
         /// and custom unique events can be added. Typical values to add are UserID, SteamID and other attributes that uniquely 
         /// identify an user. This list is persistent, meaning that events will not be removed upon Send() (like for summed events).
         /// For non-standard unique events, server side configuration needs to be done. 
-        /// Please refer to the <see href="https://support.backtrace.io">online documentation</see>.
+        /// Please refer to the <see href="https://docs.saucelabs.com/error-reporting/platform-integrations/unity/metrics/">online documentation</see>.
         /// </summary>
         //LinkedList<UniqueEvent> UniqueEvents { get; }
 
@@ -55,14 +55,14 @@ namespace Backtrace.Unity.Interfaces
         /// Adds a summed event to the outgoing queue.
         /// </summary>
         /// See <see cref="BacktraceClient.Metrics.Send(string, IDictionary)"/>.
-        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://support.backtrace.io" > online documentation</see>.</param>
+        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://docs.saucelabs.com/error-reporting/platform-integrations/unity/metrics/" > online documentation</see>.</param>
         /// <returns>true if added successfully, otherwise false.</returns>
         bool AddSummedEvent(string metricsGroupName);
 
         /// <summary>
         /// Adds a summed event to the outgoing queue.
         /// </summary>
-        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://support.backtrace.io" > online documentation</see>.</param>
+        /// <param name="metricGroupName">The name of the metric group to be incremented. This metric group must be configured on server side as well, please refer to the <see href = "https://docs.saucelabs.com/error-reporting/platform-integrations/unity/metrics/" > online documentation</see>.</param>
         /// <param name="attributes">Custom attributes to add. Will be merged with the default attributes, with attribute values provided here overriding any defaults.</param>
         /// <returns>true if added successfully, otherwise false.</returns>
         bool AddSummedEvent(string metricsGroupName, IDictionary<string, string> attributes);
