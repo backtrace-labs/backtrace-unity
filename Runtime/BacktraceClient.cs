08972c065ad2f87f5344501c49f1368a7bdfc3ec
melekr <melekramki@gmail.com>
2025-12-03 17:06:57 -0500

    Bump version

--- Runtime/BacktraceClient.cs
+++ Runtime/BacktraceClient.cs
@@ -24,7 +24,7 @@ namespace Backtrace.Unity
     /// </summary>
     public class BacktraceClient : MonoBehaviour, IBacktraceClient
     {
-        public const string VERSION = "3.14.1";
+        public const string VERSION = "3.15.0";
         internal const string DefaultBacktraceGameObjectName = "BacktraceClient";
         public BacktraceConfiguration Configuration;
 
